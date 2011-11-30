d6672d284ca7ade723a4f43187e912eaff00e416
Ido Magal <ido@idomagal.com>
2011-11-30 00:47:23 -0800

    Completed frame version

--- tabula-rasa.el
+++ tabula-rasa.el
@@ -1,11 +1,5 @@
 ;;; tr-mode.el - Distraction free editing mode
 
-;;(require 'cl)
-
-;; ------ configuration -----
-(defvar tr-face-foreground "NavajoWhite"
-  "The foreground color of the default face")
-
 (defcustom tr-width 80
   "Width of writing space."
   :type 'integer
@@ -13,10 +7,18 @@
   :set (lambda (symbol value)
          (progn (setq tr-width value)
                 (tr-update-window)))
-  :initialize 'custom-initialize-default
-)
+  :initialize 'custom-initialize-default)
 
-;; -------- code start -------
+
+(defface tabula-rasa
+  '((t (
+     :family "IM FELL English Pro"
+     :foreground "black"
+     :background "ivory"
+     :height 120
+    )))
+  "Face for tabula-rasa mode."
+  :group 'tabula-rasa)
 
 (defun tr-update-window()
   (cond 
@@ -28,13 +30,7 @@
     (set-window-margins tr-window 0 0)
     (set-window-margins (next-window) 0 0))))
 
-(defun tr-buffer-face()
-  (interactive)
-  (setq buffer-face-mode-face '(:height 160
-                                :family "IM FELL English Pro"
-                                :foreground "black"
-                                :background "papaya whip"
-                                )))
+(setq tr-mode-enabledp nil)
 
 (defun tr-mode ()
   (interactive)
@@ -44,7 +40,7 @@
 
 (defun tr-mode-enable()
   (interactive)
-  (setq tr-frame (make-frame '(
+  (setq tr-frame (make-frame `(
                               (fullscreen . fullboth)
                               (unsplittable . t)
                               (vertical-scroll-bars . nil)
@@ -53,35 +49,32 @@
                               (tool-bar-lines . 0)
                               (menu-bar-lines . 0)
                               (line-spacing . 5)
+                              (font . ,(face-font "tabula-rasa"))
+                              (foreground-color . ,(face-attribute 'tabula-rasa :foreground))
+                              (background-color . ,(face-attribute 'tabula-rasa :background))
                               )))
+
   (setq tr-window (frame-selected-window tr-frame))
 
   (set-window-dedicated-p tr-window t)
-;  (add-hook 'window-configuration-change-hook 'tr-update-window t nil)
-;  (tr-buffer-face)
-;  (buffer-face-mode 1)
+  (add-hook 'window-configuration-change-hook 'tr-update-window t nil)
 
   (tr-update-window)
 
   (setq tr-mode-enabledp t)
-;  (make-local-variable 'tr-mode-enabledp)
   (message (format "tr mode enabled on %s" (selected-frame)))
 )
 
 (defun tr-mode-disable()
   (interactive)
   (delete-frame tr-frame)
-;  (remove-hook 'window-configuration-change-hook 'tr-update-window)
-;  (buffer-face-mode 0)
+  (remove-hook 'window-configuration-change-hook 'tr-update-window)
   (setq tr-mode-enabledp nil)
   (message (format "tr mode disabled on %s" (selected-frame)))
-
 )
 
 ;;;;;;;;;;;;;;;;; end ;;;;;;;;;;;;;;;;;
 
 (provide 'tr-mode)
 
-;; todo: why doesn't fullscreen work?
-;; todo: make a proper face
 ;; todo: why doesn't tr.el load?
