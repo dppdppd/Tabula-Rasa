05a5b2c6370c01a48cd24e1c949c7e6d23fe89a9
Ido Magal <ido@idomagal.com>
2011-12-11 22:44:41 -0800

    Made a proper minor mode

--- tabula-rasa.el
+++ tabula-rasa.el
@@ -1,61 +1,64 @@
 ;;; tabula-rasa-mode.el - Distraction free writing
 
-(defcustom tr-width 80
+(defcustom tabula-rasa-width 80
   "Width of writing space."
   :type 'integer
   :group 'tabula-rasa
   :set (lambda (symbol value)
-         (progn (setq tr-width value)
-                (tr-update-window)))
+         (progn (setq tabula-rasa-width value)
+                (tabula-rasa-update-window)))
   :initialize 'custom-initialize-default)
 
 (defface tabula-rasa
   '((t (
-     :family "IM FELL English Pro"
-     :foreground "black"
-     :background "ivory"
      :height 140
     )))
   "Face for tabula-rasa mode."
   :group 'tabula-rasa)
 
-(defun tr-update-window()
+(defun tabula-rasa-update-window()
+;;  (interactive)
   (cond 
-   ((not (frame-live-p tr-frame))
-    (tr-mode-disable))
-   ((or (one-window-p t tr-frame) (window-full-width-p tr-window))
-    (set-window-margins tr-window
-                        (/ (- (frame-width tr-frame) tr-width) 2)              
-                        (/ (- (frame-width tr-frame) tr-width) 2)))
+   ((not (frame-live-p tabula-rasa-frame))
+    (tabula-rasa-mode-disable))
+   ((or (one-window-p t tabula-rasa-frame) (window-full-width-p tabula-rasa-window))
+    (set-window-margins tabula-rasa-window
+                        (/ (- (frame-width tabula-rasa-frame) tabula-rasa-width) 2)              
+                        (/ (- (frame-width tabula-rasa-frame) tabula-rasa-width) 2)))
    (t
-    (set-window-margins tr-window 0 0)
+    (set-window-margins tabula-rasa-window 0 0)
     (set-window-margins (next-window) 0 0))))
 
 ;width test: 80 chars
 ;12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
 
-(setq tr-mode-enabledp nil)
-(setq tr-frame nil)
+(setq tabula-rasa-mode nil)
 
-(defun tr-mode ()
-  (interactive)
-  (cond 
-   (tr-mode-enabledp (tr-mode-disable))
-   (t (tr-mode-enable))))
+(define-minor-mode tabula-rasa-mode
+  "YADFM: Yet Another Distraction Free Writing Mode" 
+  :lighter " TR"
+  :init-value nil
+  :group 'tabula-rasa
+; Awkwardly backwards logic. It would appear that define-minor-mode toggles the mode variable prior to evaluating the body.
+  (if tabula-rasa-mode
+      (tabula-rasa-mode-enable)
+    (tabula-rasa-mode-disable)))
+
+(setq tabula-rasa-frame nil)
 
 ;; Emacs bug? It takes 2 calls to set bg color to set frame margin colors.
-(defun tr-set-frame-parms ()
+(defun tabula-rasa-set-frame-parms ()
   (interactive)
-  (modify-frame-parameters tr-frame 
+  (modify-frame-parameters tabula-rasa-frame 
                            `(
                              (foreground-color . ,(face-attribute 'tabula-rasa :foreground))
                              (background-color . ,(face-attribute 'tabula-rasa :background))
                              )))
 
-(defun tr-mode-enable()
+(defun tabula-rasa-mode-enable()
 ;;  (interactive)
   (global-highline-mode 0)
-  (setq tr-frame (make-frame `(
+  (setq tabula-rasa-frame (make-frame `(
                                (fullscreen . fullboth)
                                (unsplittable . t)
                                (left-fringe . 0)
@@ -68,26 +71,25 @@
                                (foreground-color . ,(face-attribute 'tabula-rasa :foreground))
                                (background-color . ,(face-attribute 'tabula-rasa :background))
                                )))
-  (tr-set-frame-parms)
-  (setq tr-window (frame-selected-window tr-frame))
-  (add-hook 'window-configuration-change-hook 'tr-update-window t nil)
-  (add-hook 'kill-buffer-hook 'tr-mode-disable)
-  (add-hook 'delete-frame-functions 'tr-mode-disable)
-  (setq tr-mode-enabledp t)
-  (tr-update-window)
-
+  (setq tabula-rasa-window (frame-selected-window tabula-rasa-frame))
+  (tabula-rasa-set-frame-parms)
+  (add-hook 'window-configuration-change-hook 'tabula-rasa-update-window t nil)
+  (add-hook 'delete-frame-functions 'tabula-rasa-mode-disable)
+  (ns-toggle-fullscreen)
+  (tabula-rasa-update-window)
 )
 
-(defun tr-mode-disable()
+(defun tabula-rasa-mode-disable()
 ;;  (interactive)
-  (remove-hook 'window-configuration-change-hook 'tr-update-window)
-  (remove-hook 'kill-buffer-hook 'tr-mode-disable)  
-  (remove-hook 'delete-frame-functions 'tr-mode-disable)
-  (delete-frame tr-frame)
+;  (ns-toggle-fullscreen)
+  (remove-hook 'window-configuration-change-hook 'tabula-rasa-update-window)
+  (remove-hook 'delete-frame-functions 'tabula-rasa-mode-disable)
+  (delete-frame tabula-rasa-frame)
   (global-highline-mode 1)
-  (setq tr-mode-enabledp nil)
 )
 
 ;;;;;;;;;;;;;;;;; end ;;;;;;;;;;;;;;;;;
 
 (provide 'tabula-rasa)
+
+;;* next tabula-rasa-mode is buffer-local so if you change the buffer displayed, it breaks toggling the mode.
