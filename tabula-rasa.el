ac983e04d3229a65c5fd9f0174dd5878ce94e40e
Ido Magal <ido@idomagal.com>
2011-12-14 09:00:53 -0800

    added ability to save/restore minor mode states

--- tabula-rasa.el
+++ tabula-rasa.el
@@ -1,5 +1,10 @@
 ;;; tabula-rasa-mode.el - Distraction free writing
 
+(defgroup tabula-rasa nil
+  "Distraction free writing"
+  :version 0.1
+  :group 'text)
+
 (defcustom tabula-rasa-width 80
   "Width of writing space."
   :type 'integer
@@ -9,9 +14,39 @@
                 (tabula-rasa-update-window)))
   :initialize 'custom-initialize-default)
 
-(defface tabula-rasa
+(defcustom tabula-rasa-line-spacing 5
+  "Vertical line spacing."
+  :type 'integer
+  :group 'tabula-rasa)
+
+(defcustom tabula-rasa-minor-mode-states
+  '(("global-highline-mode" . nil) ("global-highlight-parentheses-mode" . nil))
+  "Alist of minor modes and their states while tabula-rasa mode is active. This allows you to specify modes to force on or off while in Tabula Rasa Mode."
+  :type '(repeat (cons :format "%v"
+                       (string :tag "Minor Mode")
+                       (boolean :tag "State"))))  
+
+(defface tabula-rasa-default
   '((t (
-     :height 140
+        :font "Monaco"
+        :height 140
+    )))
+  "Face for tabula-rasa mode."
+  :group 'tabula-rasa)
+
+(defface tabula-rasa-cursor
+  '((t (
+        :inherit tabula-rasa-default
+        :inverse-video t
+    )))
+  "Face for tabula-rasa mode."
+  :group 'tabula-rasa)
+
+(defface tabula-rasa-region
+  '((t (
+        :inherit tabula-rasa-default
+        :foreground "black"
+        :background "gray"
     )))
   "Face for tabula-rasa mode."
   :group 'tabula-rasa)
@@ -39,6 +74,7 @@
   :lighter " TR"
   :init-value nil
   :group 'tabula-rasa
+  :global t
 ; Awkwardly backwards logic. It would appear that define-minor-mode toggles the mode variable prior to evaluating the body.
   (if tabula-rasa-mode
       (tabula-rasa-mode-enable)
@@ -48,16 +84,75 @@
 
 ;; Emacs bug? It takes 2 calls to set bg color to set frame margin colors.
 (defun tabula-rasa-set-frame-parms ()
-  (interactive)
+;;  (interactive)
   (modify-frame-parameters tabula-rasa-frame 
                            `(
-                             (foreground-color . ,(face-attribute 'tabula-rasa :foreground))
-                             (background-color . ,(face-attribute 'tabula-rasa :background))
+                             (background-color . ,(face-attribute 'tabula-rasa-default :background))
                              )))
+
+(defun tabula-rasa-save-mmodes ()
+;;  (interactive)
+  (setq tr-saved-mmodes '())
+  (mapc (lambda (mode)
+          (if (intern-soft (car mode))
+              (setq tr-saved-mmodes (cons (list (car mode) (symbol-value (intern (car mode)))) tr-saved-mmodes))))
+        tabula-rasa-minor-mode-states)
+  (message "%s" tr-saved-mmodes))
+
+(defun tabula-rasa-save-mmodes-alist ()
+;;  (interactive)
+  (setq tr-saved-mmodes '())
+  (mapc (lambda (mode)
+          (if (intern-soft (car mode))
+              (setq tr-saved-mmodes (cons (cons (car mode) (symbol-value (intern (car mode)))) tr-saved-mmodes))))
+        tabula-rasa-minor-mode-states)
+  (message "%s" tr-saved-mmodes))
+
+
+(defun tabula-rasa-set-mmodes (mmodes-alist)
+;;  (interactive)
+  (mapc (lambda (mode)
+          (message "\n\nmode: %s" (cdr mode))
+          (message "mode assoc: %s" (assoc mode mmodes-alist))
+          (cond
+           ((cdr mode) 
+            (progn 
+               (message "t %s" (concat (car mode) " 1"))
+               (eval (read (concat "(" (car mode) " 1)")))
+            ))
+           (t
+            (progn 
+               (message "nil %s" (concat (car mode) " 0"))
+               (eval (read (concat "(" (car mode) " 0)")))
+            ))))
+        mmodes-alist))
+
+
+(defun tabula-rasa-load-mmodes ()
+;;  (interactive)
+  (mapc (lambda (mode)
+          (cond
+           ((nth 1 mode) 
+            (progn 
+               (message "%s" (concat (car mode) " 1"))
+               (eval (concat (car mode) " 1"))
+            ))
+           (t
+            (progn 
+               (message "%s" (concat (car mode) " 0"))
+               (eval (concat (car mode) " 0"))
+            ))))
+        tr-saved-mmodes))
 
 (defun tabula-rasa-mode-enable()
 ;;  (interactive)
-  (global-highline-mode 0)
+
+(tabula-rasa-save-mmodes)
+(tabula-rasa-set-mmodes tabula-rasa-minor-mode-states)
+
+;  (setq ns-antialias-text t)
+;;
+
   (setq tabula-rasa-frame (make-frame `(
                                (fullscreen . fullboth)
                                (unsplittable . t)
@@ -66,11 +161,16 @@
                                (tool-bar-lines . 0)
                                (menu-bar-lines . 0)
                                (vertical-scroll-bars . nil)
-                               (line-spacing . 5)
-                               (font . ,(face-font "tabula-rasa"))
-                               (foreground-color . ,(face-attribute 'tabula-rasa :foreground))
-                               (background-color . ,(face-attribute 'tabula-rasa :background))
+                               (line-spacing . ,tabula-rasa-line-spacing)
+                               (font . ,(face-font "tabula-rasa-default"))
+                               (foreground-color . ,(face-attribute 'tabula-rasa-default :foreground))
+                               (background-color . ,(face-attribute 'tabula-rasa-default :background))
+                               (cursor-color . ,(face-attribute 'tabula-rasa-cursor :foreground))
                                )))
+
+  (set-face-foreground 'region (face-attribute 'tabula-rasa-region :foreground) tabula-rasa-frame)
+  (set-face-background 'region (face-attribute 'tabula-rasa-region :background) tabula-rasa-frame)
+
   (setq tabula-rasa-window (frame-selected-window tabula-rasa-frame))
   (tabula-rasa-set-frame-parms)
   (add-hook 'window-configuration-change-hook 'tabula-rasa-update-window t nil)
@@ -81,11 +181,15 @@
 
 (defun tabula-rasa-mode-disable()
 ;;  (interactive)
-;  (ns-toggle-fullscreen)
+  (select-frame tabula-rasa-frame)
+  (ns-toggle-fullscreen)
   (remove-hook 'window-configuration-change-hook 'tabula-rasa-update-window)
   (remove-hook 'delete-frame-functions 'tabula-rasa-mode-disable)
   (delete-frame tabula-rasa-frame)
-  (global-highline-mode 1)
+
+(tabula-rasa-set-mmodes tr-saved-mmodes)
+;  (setq ns-antialias-text nil)
+
 )
 
 ;;;;;;;;;;;;;;;;; end ;;;;;;;;;;;;;;;;;
