ad704610080adc095f85eb63c327edecb494baaf
Ido Magal <ido@idomagal.com>
2011-12-19 00:57:24 -0800

    added description. cleaned it up

--- tabula-rasa.el
+++ tabula-rasa.el
@@ -1,4 +1,4 @@
-;;; tabula-rasa-mode.el - Distraction free writing
+;;; tabula-rasa-mode.el - Distraction free writing mode
 
 ;; Copyrigth (C) 2011  Ido Magal
 
@@ -9,6 +9,21 @@
 ;; Project page: 
 
 ;; This file is *NOT* part of GNU Emacs.
+
+;;; Commentary:
+
+;; Tabula Rasa was inspired by darkroom-mode.el, WriteRoom, and all of the other 
+;; distraction free tools. It was developed out of the need for a more customizable
+;; distraction free mode for Emacs.
+
+;;; Installation
+
+;; To use this mode, put this file in your common lisp files dir (e.g. .emacs.d/site-lisp)
+;; and put the following in your Emacs configuration file (e.g. .emacs) file:
+;; (require 'tabula-rasa)
+;; Type M-x tabula-rasa-mode to toggle the mode.
+
+;;; License:
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -23,7 +38,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING. If not, write to the
 ;; Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-
 
 ;; TODO
 ;; 
@@ -40,18 +54,29 @@
   :type 'integer
   :group 'tabula-rasa
   :set (lambda (symbol value)
-         (progn (setq tabula-rasa-width value)
-                (tabula-rasa-update-window)))
+         (setq tabula-rasa-width value)
+         (tabula-rasa-update-window))
   :initialize 'custom-initialize-default)
 
 (defcustom tabula-rasa-line-spacing 5
   "Vertical line spacing."
   :type 'integer
+  :group 'tabula-rasa
+  ;; :set (lambda (symbol value)
+  ;;        (setq tabula-rasa-line-spacing value)
+  ;;        (modify-frame-parameters tabula-rasa-frame '((line-spacing . value))))
+  :initialize 'custom-initialize-default)
+
+(defcustom tabula-rasa-toggle-antialiasing nil
+  "Whether or not to toggle anti-aliasing on text.
+Recommended for non-monotype, 'pretty' fonts."
+  :type 'boolean
   :group 'tabula-rasa)
 
 (defcustom tabula-rasa-minor-mode-states
   '(("global-highline-mode" . nil) ("global-highlight-parentheses-mode" . nil))
-  "This allows you to temporarily disable or enable minor modes for Tabula Rasa. Add the minor mode and the desired state while in Tabula Rasa mode."
+  "This allows you to temporarily disable or enable minor modes for Tabula Rasa.
+Add the minor mode and the desired state while in Tabula Rasa mode."
   :type '(repeat (cons :format "%v"
                        (string :tag "Minor Mode")
                        (boolean :tag "State"))))  
@@ -92,23 +117,8 @@
   "Face for the Tabula Rasa region select. Only background and foreground colors matter."
   :group 'tabula-rasa)
 
-(defun tabula-rasa-update-window()
-;;  (interactive)
-  (cond 
-   ((not (frame-live-p tabula-rasa-frame))
-    (tabula-rasa-mode-disable))
-   ((or (one-window-p t tabula-rasa-frame) (window-full-width-p tabula-rasa-window))
-    (set-window-margins tabula-rasa-window
-                        (/ (- (frame-width tabula-rasa-frame) tabula-rasa-width) 2)              
-                        (/ (- (frame-width tabula-rasa-frame) tabula-rasa-width) 2)))
-   (t
-    (set-window-margins tabula-rasa-window 0 0)
-    (set-window-margins (next-window) 0 0))))
-
-;width test: 80 chars
-;12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
-
 (setq tabula-rasa-mode nil)
+(setq tabula-rasa-frame nil)
 
 (define-minor-mode tabula-rasa-mode
   "YADFM: Yet Another Distraction Free Writing Mode" 
@@ -119,9 +129,23 @@
 ; Awkwardly backwards logic. It would appear that define-minor-mode toggles the mode variable prior to evaluating the body.
   (if tabula-rasa-mode
       (tabula-rasa-mode-enable)
-    (tabula-rasa-mode-disable)))
+    (tabula-rasa-mode-disable t)))
 
-(setq tabula-rasa-frame nil)
+(defun tabula-rasa-update-window()
+;;  (interactive)
+  (cond 
+   ((not (frame-live-p tabula-rasa-frame))
+    (tabula-rasa-mode-disable nil))
+   ((or (one-window-p t tabula-rasa-frame) (window-full-width-p tabula-rasa-window))
+    (set-window-margins tabula-rasa-window
+                        (/ (- (frame-width tabula-rasa-frame) tabula-rasa-width) 2)              
+                        (/ (- (frame-width tabula-rasa-frame) tabula-rasa-width) 2)))
+   (t
+    (set-window-margins tabula-rasa-window 0 0)
+    (set-window-margins (next-window) 0 0))))
+
+;width test: 80 chars
+;12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
 
 ;; Emacs bug? It takes 2 calls to set bg color to set frame margin colors.
 (defun tabula-rasa-set-frame-parms ()
@@ -154,9 +178,13 @@
       (progn
         (message "Tabula Rasa mode is already running")
         (exit)))
+;; minor modes adjustment
   (tabula-rasa-save-mmodes)
   (tabula-rasa-set-mmodes tabula-rasa-minor-mode-states)
-  ;;  (setq ns-antialias-text t)
+;; antialiasing
+  (if tabula-rasa-toggle-antialiasing
+      (setq ns-antialias-text (not ns-antialias-text)))
+
   (setq tabula-rasa-frame (make-frame `(
                                         (fullscreen . fullboth)
                                         (unsplittable . t)
@@ -178,8 +206,10 @@
   (setq tabula-rasa-window (frame-selected-window tabula-rasa-frame))
   (tabula-rasa-set-frame-parms)
   (add-hook 'window-configuration-change-hook 'tabula-rasa-update-window t nil)
-  (add-hook 'delete-frame-functions 'tabula-rasa-mode-disable)
-  
+  (add-hook 'delete-frame-functions (
+                                     lambda (frame)
+                                            (if (eq frame tabula-rasa-frame)
+                                                  (tabula-rasa-mode-disable nil))))
   (select-frame tabula-rasa-frame)
   (cond 
    ((string= system-type "darwin")
@@ -194,31 +224,37 @@
   
   (tabula-rasa-update-window))
 
-(defun tabula-rasa-mode-disable()
+(defun tabula-rasa-mode-disable(del-frame)
 ;;  (interactive)
-
-  (select-frame tabula-rasa-frame)
-  ;; (cond 
-  ;;  ((string= system-type "darwin")
-  ;;   (ns-toggle-fullscreen))
-  ;;  ((string= system-type "gnu/linux")
-  ;;   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-  ;;                          '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
-  ;;  ((string= system-type "windows-nt")
-  ;;   (if (fboundp 'w32-send-sys-command)
-  ;;       ;; WM_SYSCOMMAND restore #xf120
-  ;;       (w32-send-sys-command 61728)
-  ;;     (progn (set-frame-parameter tabula-rasa-frame 'width 82)
-  ;;            (set-frame-parameter tabula-rasa-frame 'fullscreen 'fullheight)))))
-
-  (remove-hook 'window-configuration-change-hook 'tabula-rasa-update-window)
-  (remove-hook 'delete-frame-functions 'tabula-rasa-mode-disable)
-  (delete-frame tabula-rasa-frame)
-  (tabula-rasa-set-mmodes tr-saved-mmodes)
-  ;;  (setq ns-antialias-text nil)
-)
+  (if (frame-live-p tabula-rasa-frame)
+      (progn
+        (if tabula-rasa-toggle-antialiasing
+            (progn
+              (setq ns-antialias-text (not ns-antialias-text))
+              ;; On OSX, toggling antialiasing doesn't refresh the entire display so
+              ;; we force it.
+              (redraw-display)))
+        (setq tabula-rasa-mode nil)
+        (select-frame tabula-rasa-frame)
+        (cond 
+         ((string= system-type "darwin")
+          (ns-toggle-fullscreen))
+         ((string= system-type "gnu/linux")
+          (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                                 '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+         ((string= system-type "windows-nt")
+          (if (fboundp 'w32-send-sys-command)
+              ;; WM_SYSCOMMAND restore #xf120
+              (w32-send-sys-command 61728)
+            (progn (set-frame-parameter tabula-rasa-frame 'width 82)
+                   (set-frame-parameter tabula-rasa-frame 'fullscreen 'fullheight)))))
+        
+        (remove-hook 'window-configuration-change-hook 'tabula-rasa-update-window)
+        (remove-hook 'delete-frame-functions 'tabula-rasa-mode-disable)
+        (if del-frame (delete-frame tabula-rasa-frame))
+        (tabula-rasa-set-mmodes tr-saved-mmodes)
+        )))
 
 ;;;;;;;;;;;;;;;;; end ;;;;;;;;;;;;;;;;;
 
 (provide 'tabula-rasa)
-
