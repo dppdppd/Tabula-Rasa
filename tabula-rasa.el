1fc2126018e362129a99197fa8b5b4950ae26ce4
Ido Magal <ido@idomagal.com>
2011-12-19 21:25:32 -0800

    comment tweaks

--- tabula-rasa.el
+++ tabula-rasa.el
@@ -1,39 +1,39 @@
-;;; tabula-rasa-mode.el - Distraction free writing mode
+;;; tabula-rasa-mode.el --- Distraction free writing mode
 
 ;; Copyrigth (C) 2011  Ido Magal
 
 ;; Author: Ido Magal <M8R-u8t2l4(at)mailinator.com>
 ;; Version: 0.1.0
-;; Keywords; distraction free, writing
+;; Keywords: distraction free, writing
 ;; URL: https://github.com/idomagal/Tabula-Rasa/blob/master/tabula-rasa.el
 
 ;; This file is *NOT* part of GNU Emacs.
 
 ;;; Commentary:
 
-;; Tabula Rasa was inspired by darkroom-mode.el, WriteRoom, and all of the other 
-;; distraction free tools. It was developed out of the need for a more customizable
-;; distraction free mode for Emacs.
-;;
-;; Fullscreen caveats:
-;; Win32:       supports maximized window, not total fullscreen yet.
-;; Mac OSX:     supports fullscreen with cocoa (ns-toggle-fullscreen)
+;; Tabula Rasa was inspired by darkroom-mode.el, WriteRoom, and all of the other  
+;; distraction free tools. It was developed out of the need for a more customizable  
+;; distraction free mode for Emacs.  
 
-;;; Installation
+;; Fullscreen caveats:  
+;; Win32:       supports maximized window, not total fullscreen yet.  
+;; Mac OSX:     supports fullscreen with cocoa (ns-toggle-fullscreen)  
 
-;; To use this mode, put this file in your common lisp files dir (e.g. .emacs.d/site-lisp)
+;;; Installation  
+
+;; To use this mode, put this file in your common lisp files dir (e.g. .emacs.d/site-lisp)  
 ;; and put the following in your Emacs configuration file (e.g. .emacs) file:
-;; (require 'tabula-rasa)
-;; Type M-x tabula-rasa-mode to toggle the mode.
-;;
-;; For customization of colors, etc, type M-x customize-group ENT tabula-rasa ENT
-;; Customization options include:
-;; - Text font and colors.
-;; - Cursor and region colors.
-;; - Column width
-;; - Line spacing
-;; - Minor modes to enable or disable for Tabula Rasa
-;; - Whether or not to antialias text (experimental)
+;; (require 'tabula-rasa)  
+;; Type M-x tabula-rasa-mode to toggle the mode.  
+
+;; For customization of colors, etc, type M-x customize-group RET tabula-rasa RET
+;; Customization options include:  
+;; - Text font and colors.  
+;; - Cursor and region colors.  
+;; - Column width  
+;; - Line spacing  
+;; - Minor modes to enable or disable for Tabula Rasa  
+;; - Whether or not to antialias text (experimental)  
 
 ;;; License:
 
